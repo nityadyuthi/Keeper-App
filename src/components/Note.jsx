f207452a9e1c04051f971564f4aba1972125e2db
Avadhanam Nitya Dyuthi <42372495+nityadyuthi@users.noreply.github.com>
2020-09-16 01:53:27 +0530

    Add Project Files

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Add title of note here</h1>
+      <p>What's the note?</p>
+    </div>
+  );
+}
+
+export default Note;
